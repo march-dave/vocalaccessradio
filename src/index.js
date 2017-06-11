7842de2f5a77ab904dbe35646d7595f15927a9e2
march-dave <ysangy@gmail.com>
2017-06-11 10:23:13 -0400

    first init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import store from './stores'
+import { Provider } from 'react-redux'
+import Intro from './components/presentation/Intro'
+
+const app = (
+	<Provider store={store.configure(null)}>
+		<Intro />
+	</Provider>
+)
+
+
+ReactDOM.render(app, document.getElementById('root'))
